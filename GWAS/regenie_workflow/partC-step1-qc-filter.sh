802d4860b204bd0dda65d475171e4d28460f5bc0
biochen4445 <hlchen0519@gmail.com>
2022-04-27 23:52:43 +0800

    Update partC-step1-qc-filter.sh
    revise the plink files from v1 to v2
    modify the sh script

--- GWAS/regenie_workflow/partC-step1-qc-filter.sh
+++ GWAS/regenie_workflow/partC-step1-qc-filter.sh
@@ -10,7 +10,7 @@ data_field="22418"
 
 # How to Run:
 # Run this shell script using: 
-# sh 02-step1-qc-filter.sh 
+# sh partC-step1-qc-filter.sh 
 # on the command line on your machine
 
 
@@ -23,15 +23,15 @@ data_field="22418"
 #set output directory (also location of merged files)
 data_file_dir="/Data/"
 
-run_plink_qc="plink2 --bfile ukb${data_field}_c1_22_v1_merged\
+run_plink_qc="plink2 --bfile ukb${data_field}_c1_22_v2_merged\
  --keep diabetes_summaryICD_wes_450k.phe --autosome\
  --maf 0.01 --mac 20 --geno 0.1 --hwe 1e-15\
  --mind 0.1 --write-snplist --write-samples\
  --no-id-header --out  WES_array_snps_qc_pass"
 
-dx run swiss-army-knife -iin="${data_file_dir}/ukb${data_field}_c1_22_v1_merged.bed" \
-   -iin="${data_file_dir}/ukb${data_field}_c1_22_v1_merged.bim" \
-   -iin="${data_file_dir}/ukb${data_field}_c1_22_v1_merged.fam"\
+dx run swiss-army-knife -iin="${data_file_dir}/ukb${data_field}_c1_22_v2_merged.bed" \
+   -iin="${data_file_dir}/ukb${data_field}_c1_22_v2_merged.bim" \
+   -iin="${data_file_dir}/ukb${data_field}_c1_22_v2_merged.fam"\
    -iin="${data_file_dir}/diabetes_summaryICD_wes_450k.phe" \
    -icmd="${run_plink_qc}" --tag="Step1" --instance-type "mem1_ssd1_v2_x16"\
    --destination="${project}:/Data/" --brief --yes
