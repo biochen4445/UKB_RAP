dbd564f27a233140513432539656f97905f99570
biochen4445 <hlchen0519@gmail.com>
2022-04-27 23:58:57 +0800

    Update partF-step2-regenie.sh

--- GWAS/regenie_workflow/partF-step2-regenie.sh
+++ GWAS/regenie_workflow/partF-step2-regenie.sh
@@ -22,9 +22,9 @@ $project=$1
 
 # Additional inputs
 # for each chromosome, you will run a separate worker
-# - /{exome_file_dir}/ukb23155_c1_b0_v1.bed - Chr1 file for 200k release
-# - /{exome_file_dir}/ukb23155_c1_b0_v1.bim 
-# - /{exome_file_dir}/ukb23155_c1_b0_v1.bam 
+# - /{exome_file_dir}/ukb23155_c1_b0_v2.bed - Chr1 file for 450k release
+# - /{exome_file_dir}/ukb23155_c1_b0_v2.bim 
+# - /{exome_file_dir}/ukb23155_c1_b0_v2.bam 
 # - /Data/WES_c1_snps_qc_pass.snplist - from Part E
 
 # Outputs (for each chromosome):
@@ -39,7 +39,7 @@ data_file_dir="${project}:/Data/"
 
 
 for chr in {1..2}; do
-  run_regenie_cmd="regenie --step 2 --bed ukb${data_field}_c${chr}_b0_v1 --out assoc.c${chr}\
+  run_regenie_cmd="regenie --step 2 --bed ukb${data_field}_c${chr}_b0_v2 --out assoc.c${chr}\
     --phenoFile diabetes_summaryICD_wes_450k.phe --covarFile diabetes_summaryICD_wes_450k.phe\
     --bt --approx --firth-se --firth --extract WES_c${chr}_snps_qc_pass.snplist\
     #phenotype colum and covariate columns in diabetes_wes_200k.phe\
@@ -47,9 +47,9 @@ for chr in {1..2}; do
     --pred diabetes_results_pred.list --bsize 200\
     --pThresh 0.05 --minMAC 3 --threads 16 --gz"
 
-  dx run swiss-army-knife -iin="${exome_file_dir}/ukb${data_field}_c${chr}_b0_v1.bed" \ #bed file for chromosome
-   -iin="${exome_file_dir}/ukb${data_field}_c${chr}_b0_v1.bim" \ #bim file for chromosome
-   -iin="${exome_file_dir}/ukb2${data_field}_c${chr}_b0_v1.fam"\ #fam file for chromosome
+  dx run swiss-army-knife -iin="${exome_file_dir}/ukb${data_field}_c${chr}_b0_v2.bed" \ #bed file for chromosome
+   -iin="${exome_file_dir}/ukb${data_field}_c${chr}_b0_v2.bim" \ #bim file for chromosome
+   -iin="${exome_file_dir}/ukb2${data_field}_c${chr}_b0_v2.fam"\ #fam file for chromosome
    -iin="${data_file_dir}/WES_c${chr}_snps_qc_pass.snplist"\ #individual QC snplist produced with 04-step2-qc-filter.sh
    -iin="${data_file_dir}/diabetes_summaryICD_wes_450k.phe" \  #phenotype results
    -iin="${data_file_dir}/diabetes_results_pred.list" \ #Merged results produced from 03-step1-regenie.sh
