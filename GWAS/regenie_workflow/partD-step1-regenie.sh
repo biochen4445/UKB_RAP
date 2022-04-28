b94b4b9e3ea2ec05b42982a799a1185393400cfb
biochen4445 <hlchen0519@gmail.com>
2022-04-28 23:20:08 +0800

    Update partD-step1-regenie.sh

--- GWAS/regenie_workflow/partD-step1-regenie.sh
+++ GWAS/regenie_workflow/partD-step1-regenie.sh
@@ -15,15 +15,14 @@
 # Note that you can adjust the output directory by setting the data_file_dir variable
 # - /Data/ddiabetes_summaryICD_wes_450k.phe - from part A (please refer to notebook & slides)
 # - /Data/WES_array_snps_qc_pass.snplist - from part C
-# - /Data/ukb22418_c1_22_merged.bed - from part B
-# - /Data/ukb22418_c1_22_merged.bed - from part B
-# - /Data/ukb22418_c1_22_merged.bed - from part B
+# - /Data/ukb22418_c1_22_v2_merged.bed - from part B
+# - /Data/ukb22418_c1_22_v2_merged.fam - from part B
+# - /Data/ukb22418_c1_22_v2_merged.dim - from part B
 
 # Outputs:
 # - /Data/diabetes_results_1.loco.gz - Leave One Chromosome Out results (used in part F)
 # - /Data/diabetes_results_pred.list - List of files generated this step (used in part F)
 # - /Data/diabetes_results.log
-
 
 #output directory - this should also be where the files in 02-step1-qc-filter.sh end up
 data_file_dir="/Data/"
@@ -35,9 +34,10 @@ run_regenie_step1="regenie --step 1\
  --covarCol age --covarCol sex --covarCol ethnic_group --covarCol ever_smoked\
  --bsize 1000 --bt --loocv --gz --threads 16";
 
-dx run swiss-army-knife -iin="${data_file_dir}/ukb22418_c1_22_v2_merged.bed" \
-   -iin="${data_file_dir}/ukb22418_c1_22_v2_merged.bim" \
-   -iin="${data_file_dir}/ukb22418_c1_22_v2_merged.fam"\
-   -iin="${data_file_dir}/diabetes_summaryICD_wes_450k.phe" \
-   -icmd=${run_plink_wes} --tag="Step1" --instance-type "mem1_ssd1_v2_x16"\
-   --destination="${project}:/Data/" --brief --yes;
+dx run swiss-army-knife -iin="${data_file_dir}ukb22418_c1_22_v2_merged.bed" \
+   -iin="${data_file_dir}ukb22418_c1_22_v2_merged.bim" \
+   -iin="${data_file_dir}ukb22418_c1_22_v2_merged.fam"\
+   -iin="${data_file_dir}WES_array_snps_qc_pass.snplist"\
+   -iin="${data_file_dir}diabetes_summaryICD_wes_450k.phe" \
+   -icmd=${run_regenie_step1} --tag="Step1" --instance-type "mem1_ssd1_v2_x16"\
+   --destination="${data_file_dir}" --brief --yes;
