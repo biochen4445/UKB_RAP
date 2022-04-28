62762c3f192920ea554ef2eda32697e61a0bfc77
biochen4445 <hlchen0519@gmail.com>
2022-04-28 23:21:17 +0800

    Update partD-step1-regenie.sh

--- GWAS/regenie_workflow/partD-step1-regenie.sh
+++ GWAS/regenie_workflow/partD-step1-regenie.sh
@@ -32,7 +32,7 @@ run_regenie_step1="regenie --step 1\
  --phenoFile diabetes_summaryICD_wes_450k.phe --covarFile diabetes_summaryICD_wes_450k.phe\
  --extract WES_array_snps_qc_pass.snplist --phenoCol diabetes_cc\
  --covarCol age --covarCol sex --covarCol ethnic_group --covarCol ever_smoked\
- --bsize 1000 --bt --loocv --gz --threads 16";
+ --bsize 1000 --bt --loocv --gz --threads 16"
 
 dx run swiss-army-knife -iin="${data_file_dir}ukb22418_c1_22_v2_merged.bed" \
    -iin="${data_file_dir}ukb22418_c1_22_v2_merged.bim" \
@@ -40,4 +40,4 @@ dx run swiss-army-knife -iin="${data_file_dir}ukb22418_c1_22_v2_merged.bed" \
    -iin="${data_file_dir}WES_array_snps_qc_pass.snplist"\
    -iin="${data_file_dir}diabetes_summaryICD_wes_450k.phe" \
    -icmd=${run_regenie_step1} --tag="Step1" --instance-type "mem1_ssd1_v2_x16"\
-   --destination="${data_file_dir}" --brief --yes;
+   --destination="${data_file_dir}" --brief --yes
